789af333b2a4c1a34c16e87c3db8aac608058640
Barabash <ash_tyle@protonmail.com>
2024-12-19 13:12:07 +0100

    Before merge

---
+++ source/NUCLEO-F446RE_prototypes/Core/Inc/ST3020_servo.h
@@ -0,0 +1,13 @@
+/*
+ * ST3020_servo.h
+ *
+ *  Created on: Dec 19, 2024
+ *      Author: Barabash
+ */
+
+#ifndef INC_ST3020_SERVO_H_
+#define INC_ST3020_SERVO_H_
+
+
+
+#endif /* INC_ST3020_SERVO_H_ */
